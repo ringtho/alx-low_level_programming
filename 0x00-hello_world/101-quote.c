628456286973040961104bb46aa59481881fb222
Smith Ringtho <sringtho@gmail.com>
2022-03-10 02:15:21 -0800

    ADD write method

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - Entry point
+ *
+ * write: print string to standard output using write
+ * Return: it should return 1 for success
+ *
+ */
+int main(void)
+{
+		write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",61);
+		return (1);
+}
