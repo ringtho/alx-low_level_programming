8d126f01348bb8ff7cdc3f8635525e84f4dca4b1
Smith Ringtho <sringtho@gmail.com>
2022-03-10 00:46:36 -0800

    minor changes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,12 @@
 /**
  * main - Entry point
  *
- * Return - A string of characters using puts()
- * The program should end with value 0
+ * puts: string output
+ * Return: Always 0 (Success)
  *
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }
