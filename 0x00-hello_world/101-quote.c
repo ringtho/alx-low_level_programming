b547b5e6e80968761dd0e8c02eca761f3e48eb0b
Smith Ringtho <sringtho@gmail.com>
2022-03-10 02:46:14 -0800

    modify

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,14 +3,14 @@
 
 /**
  *
- * main - Entry point
+ * main- Entry point
  *
- * write: print string to standard output using write
- * Return: it should return 1 for success
+ * Description: Print string to standard output using write
+ * Return: The return value should be 1 for (Success)
  *
  */
 int main(void)
 {
-	write(1,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",61);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
 	return (1);
 }
