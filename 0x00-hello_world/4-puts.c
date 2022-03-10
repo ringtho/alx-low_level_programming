8c1f63971859e38087f5e9e8fde5292223b4db87
Smith Ringtho <sringtho@gmail.com>
2022-03-10 00:35:48 -0800

    puts method

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return - A string of characters using puts()
+ * The program should end with value 0
+ *
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
