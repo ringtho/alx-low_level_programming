e0764a1709521f1aa87d1eea241b4cdee066ba3d
Smith Ringtho <sringtho@gmail.com>
2022-03-10 02:29:35 -0800

    modify

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,8 +8,8 @@
  * Return: it should return 1 for success
  *
  */
-int main(void)
+int main()
 {
-		write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",61);
+		write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 		return (1);
 }
