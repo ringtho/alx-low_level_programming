ed1ef499ce3ea1ae06770e70f2a3d0b5ea8d1bbb
Smith Ringtho <sringtho@gmail.com>
2022-03-11 00:21:35 -0800

    reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - Entry point
+ *
+ * print: alphabet in reverse order
+ * Return: always 0 (success)
+ *
+ */
+int main(void)
+{
+	char c = 'z';
+
+	while ( c >= 'a')
+	{
+		putchar(c);
+		c--;
+	}
+	putchar('\n');
+	return (0);
+}
