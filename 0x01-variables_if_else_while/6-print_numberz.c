562659f9e66ab8bc95ad6643a2d0d361051a6259
Smith Ringtho <sringtho@gmail.com>
2022-03-11 00:16:43 -0800

    print numbers using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - Entry point
+ *
+ * print: print all the numbers of base 10 using putcha
+ * Return: always 0 (success)
+ *
+ */
+int main (void)
+{
+	int num = 0;
+	while (num < 10)
+	{
+		char ch = num + '0';
+		putchar(ch);
+		num++;
+	}
+	putchar('\n');
+	return (0);
+
+}
