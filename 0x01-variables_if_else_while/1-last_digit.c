a1da647c9bcbfb052c784a4551cdaa7293d963fd
Smith Ringtho <sringtho@gmail.com>
2022-03-11 01:22:11 -0800

    minor changes for 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,10 +21,12 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, last_dig);
 	}
+
 	else if (n = 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, last_dig);
 	}
+
 	else
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_dig);
