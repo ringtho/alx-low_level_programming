5e8a9e939b4f5a14bc412af2a66040c8e8fdb63d
Smith Ringtho <sringtho@gmail.com>
2022-03-10 23:37:53 -0800

    alphabet lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - Entry point
+ *
+ * putchar: print the alphabet from a - z lowercase
+ * Return: always 0 (success)
+ *
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+
+	return (0);
+
+}
