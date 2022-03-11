4d6b69d0c4f5838a6f68254b22b92f267ad1e1e8
Smith Ringtho <sringtho@gmail.com>
2022-03-11 02:13:38 -0800

    9 permutations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for function
+ * 
+ * print: possible combinations of single didgit numbers
+ * Return: always 0 (success)
+ *
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		putchar(num + '0');
+		putchar(',');
+		putchar('\n');
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
