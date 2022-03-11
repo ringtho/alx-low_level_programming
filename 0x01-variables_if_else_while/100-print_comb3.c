e9293e7545d5374f2de5e4c2b3f8dfc51db407fa
Smith Ringtho <sringtho@gmail.com>
2022-03-11 07:34:25 -0800

    minor changes

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,27 +12,22 @@ int main(void)
 	int j;
 	int i;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 48; i <= 56; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (j = 49; j <= 57; j++)
 		{
-			if ( i != j && j != i)
+			if (j > i)
 			{
 				putchar(i + '0');
 				putchar(j + '0');
-				putchar(',');
-				putchar(' ');
-				/*if (i == 8 && j == 9)
-				{
-					putchar('\n');
-				} else
+				if (i != 56 || j != 57)
 				{
 					putchar(',');
 					putchar(' ');
-				}*/
+				}
 			}
 		}
 	}
-
+	putchar('\n');
 	return (0);
 }
