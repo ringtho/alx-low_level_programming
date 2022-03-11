7f9d74b26d8e7656efb4269d6d3a03ee575d7450
Smith Ringtho <sringtho@gmail.com>
2022-03-11 01:54:49 -0800

    small changes

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 /**
- *
  * main - Entry point
  *
  * putchar: print alphabet without letters q and e
@@ -18,6 +17,7 @@ int main(void)
 		{
 			putchar(c);
 		}
+
 		c++;
 	}
 	putchar('\n');
