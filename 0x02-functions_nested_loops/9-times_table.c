fe7ba9833f2a0cd98f3aa5e3a4386d8926882897
Smith Ringtho <sringtho@gmail.com>
2022-03-15 14:17:27 -0700

    last digit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,22 +6,33 @@
  */
 void times_table(void)
 {
-	int num = 9;
-	int i = 0;
-	int j = 0;
-	int result;
-
-	while (i < 10)
+	int rw, clm, rst;
+	
+	for (rw = 0; rw <= 9; rw++)
 	{
-		while (j < 10)
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+
+		for (clm = 1; clm <= 9; clm++)
 		{
-			result = j * num;
-			_putchar(result + '0');
-			_putchar(',');
-			j++;
+			rst = (rw * clm);
+			if ((rst / 10) > 0)
+			{
+				_putchar((rst / 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar((rst % 10) + '0');
+
+			if (clm < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
-		result = i * num;
-		_putchar(result + '0');
-		i++;
+		_putchar('\n');
 	}
 }
