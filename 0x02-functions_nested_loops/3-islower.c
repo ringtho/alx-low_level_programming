e05c093767cb9094aaf2bf3d966d1c55c30f9482
Smith Ringtho <sringtho@gmail.com>
2022-03-15 11:32:40 -0700

    3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,9 @@
 #include "main.h"
 
-
 /**
  * _islower - checks if a character is a lowercase or uppercase
  * @c: character to be tested 
  * Return: 1 if character is lowercase and 0 if otherwise
- *
  */
 int _islower(int c)
 {
