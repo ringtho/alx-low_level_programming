400fb97e01429a48360f27f147f075373ac9739b
Smith Ringtho <sringtho@gmail.com>
2022-03-15 12:08:57 -0700

    absolute number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @int: the variable of datatype int to be fed into the function
+ * Return: 0 if success
+ *
+ */
+int _abs(int)
+{
+	int num;
+
+	if (num < 0)
+	{
+		num *= -1;
+		_putchar(num);
+	}
+	else
+	{
+		_putchar(num);
+	}
+	return (0);
+}
