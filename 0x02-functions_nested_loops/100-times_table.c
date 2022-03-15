6158c297cc7804999fabedb5dfcfc22a656b6b23
Smith Ringtho <sringtho@gmail.com>
2022-03-15 15:07:40 -0700

    Add 100

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * times_table - print the 9 times table
+ * @n: the number
+ *
+ */
+void times_table(int n)
+{
+	int rw, clm, rst;
+
+	if (n <= 15 || n > 0)
+	{
+		for (rw = 0; rw <= n; rw++)
+		{
+			_putchar('0');
+			_putchar(',');
+			_putchar(' ');
+			for (clm = 1; clm <= n; clm++)
+			{
+				rst = (rw * clm);
+				if ((rst / 10) > 0)
+				{
+					_putchar((rst / 10) + '0');
+				}
+				else
+				{
+					_putchar(' ');
+				}
+				_putchar((rst % 10) + '0');
+
+				if (clm < n)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
