0b5c2de3eb43f3bcad239d2d1b931f36d35895c2
Smith Ringtho <sringtho@gmail.com>
2022-03-15 10:22:10 -0700

    0_putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * _putchar - prints the word _putchar
+ * Return: return 0 if success
+ */
+int main(void)
+{
+	char word[9] = "_putchar";
+	
+	int i = 0;
+
+	while (i < 8)
+	{
+		_putchar(word[i]);
+		i++;
+	}
+	_putchar('\n);
+	return (0);
+}
