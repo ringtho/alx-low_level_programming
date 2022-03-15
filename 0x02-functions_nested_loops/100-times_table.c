7543a4754abe0bd0ea934a6805cb905e19c145ef
Smith Ringtho <sringtho@gmail.com>
2022-03-15 15:42:59 -0700

    minor changes

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,35 +7,36 @@
  */
 void print_times_table(int n)
 {
+
 	int rw, clm, rst;
 
-	if (n <= 15 || n > 0)
+	if (n <= 15 && n >= 0)
 	{
 		for (rw = 0; rw <= n; rw++)
 		{
 			_putchar('0');
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
+			
 			for (clm = 1; clm <= n; clm++)
 			{
+				_putchar(',');
+				_putchar(' ');
+
 				rst = (rw * clm);
-				if ((rst / 10) > 0)
+				
+				if (rst <= 99)
+					_putchar(' ');
+				if (rst <= 9)
+					_putchar(' ');
+				if (rst >= 100)
+				{
+					_putchar((rst / 100) + '0');
+					_putchar(((rst / 10) % 10) + '0');
+				}
+				else if (rst <= 99 && rst >= 10)
 				{
 					_putchar((rst / 10) + '0');
 				}
-				else
-				{
-					_putchar(' ');
-				}
-				_putchar((rst % 10) + '0');
-
-				if (clm < n)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-				}
+				_putchar((prod % 10) + '0');
 			}
 			_putchar('\n');
 		}
