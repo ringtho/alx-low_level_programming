f48cd96edb22a0a646ad85f4ef1bf6b48cc146a0
Smith Ringtho <sringtho@gmail.com>
2022-03-15 13:12:02 -0700

    10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @num1: number one
+ * @num2: number two
+ * Return: result of addition
+ *
+ */
+int add(int num1, int num2)
+{
+	int result;
+
+	result = num1 + num2;
+	return (result);
+}
