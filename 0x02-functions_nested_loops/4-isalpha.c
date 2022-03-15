0715073138ff424e53a806b2291dfde8b503d11d
Smith Ringtho <sringtho@gmail.com>
2022-03-15 11:44:15 -0700

    is aplha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: the character or number to be subjected to test
+ * Return: 1 if c is a letter, lowercase or uppercase
+ * Return: 0 if c is a number
+ *
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
