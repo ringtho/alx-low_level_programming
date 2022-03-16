ce949e2aa20a80037dafe801d52c39fef800988e
Smith Ringtho <sringtho@gmail.com>
2022-03-16 09:54:09 -0700

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - test if a number is positive or negative
+ * Return: 0 for success
+ *
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
