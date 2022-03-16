688bfdc3b6c77d396e83abffd5b72fc7516bc64f
Smith Ringtho <sringtho@gmail.com>
2022-03-16 11:47:19 -0700

    minor

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
 /**
- * main - commenting out infinite loop
+ * main - causes an infinite loop
  * Return: 0 
  */
 
 int main(void)
 {
-	/* int i; */
+	int i;
 
 	printf("Infinite loop incoming :(\n");
 
-	/* i = 0;
+	i = 0;
 	 
-	 while (i < 10)
+	/* while (i < 10)
 	 {
 		putchar(i);
 	 } */
