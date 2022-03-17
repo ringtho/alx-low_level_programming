22303ae2d4bb16a06cb3ca915bf60b7ec09f09b8
Smith Ringtho <sringtho@gmail.com>
2022-03-17 12:21:12 -0700

    Fizz Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>"
+
+/**
+ * main - print numbers form 1 to 100 
+ * Return: 0 for success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3) != 0 && (i % 5) != 0)
+		{
+			printf("%d", i);
+		}
+		else
+		{
+			if (i % 3 == 0)
+				printf("Fizz");
+			if (i % 5 == 0)
+				printf("Buzz");
+		}
+		if (i < 100)
+			printf(" ");
+	}
+	putchar(10);
+	return (0);
+}
