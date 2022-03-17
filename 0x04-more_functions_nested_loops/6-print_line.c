2787479e1a8018771909531d9704401f1da28ff6
Smith Ringtho <sringtho@gmail.com>
2022-03-17 11:42:10 -0700

    minor change

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,17 +7,19 @@
  */
 void print_line(int n)
 {
+	int i;
+
 	if (n <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		int i = 0;
+		i = 0;
 
 		while (i < n)
 		{
-			_putchar("_");
+			_putchar('_');
 			i++;
 		}
 		_putchar('\n');
