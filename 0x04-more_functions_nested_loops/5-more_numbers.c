4490affc8033f5b01b7acfdad3f2d40d37afaca4
Smith Ringtho <sringtho@gmail.com>
2022-03-17 11:19:51 -0700

    print more numbers 5-

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers form 0 to 14 10 times
+ * Return: No return
+ */
+void more_numbers(void)
+{
+	int i = 0;
+	int j;
+
+	while (i <= 10)
+	{
+		j = 0;
+		while (j <= 14)
+		{
+			_putchar(j + '0');
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
