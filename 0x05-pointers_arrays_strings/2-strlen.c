7ccbb574b88a69aac2953db5267e0bd4cdd3ecfd
Smith Ringtho <sringtho@gmail.com>
2022-03-21 09:47:51 -0700

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - return the length of a string
+ * @s: the string
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while(*s++)
+	{
+		length++;
+	}
+	return (length);
