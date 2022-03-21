9a3e78af847e7bcacdf645432838edabef23bae2
Smith Ringtho <sringtho@gmail.com>
2022-03-21 09:59:31 -0700

    3 puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string 
+ * @str: the string to be printed
+ * Return: void
+ */
+void _puts(char *str)
+{
+	while (*str)
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
+}
