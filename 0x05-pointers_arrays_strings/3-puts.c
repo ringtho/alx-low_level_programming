cf4bc8e706db2da2431ff257380a4ad81c7f413a
Smith Ringtho <sringtho@gmail.com>
2022-03-21 10:04:42 -0700

    Add _putchar to main.h

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _puts - prints a string 
- * @str: the string to be printed
+ * _puts - prints a string
+ * @str: the string
  * Return: void
  */
 void _puts(char *str)
