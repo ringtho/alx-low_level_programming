a2492a2f747a245f18b1548ce1f9c6916fccac39
Smith Ringtho <sringtho@gmail.com>
2022-03-24 04:55:06 -0700

    chnages to task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,16 +7,11 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i, j, diff;
-
-	i = 0;
-	j = 0;
-
-	while (*(s1 + i) != '\0' && *(s2 + j) != '\0' && *(s1 + i) == *(s2 + j))
+	while (*s1 && *s2 && *s1 == *s2)
 	{
-		i++;
-		j++;
+		s1++;
+		s2++;
 	}
-	diff = *(s1 + i) - *(s2 + j);
-	return (diff);
+
+	return (*s1 - *s2);
 }
