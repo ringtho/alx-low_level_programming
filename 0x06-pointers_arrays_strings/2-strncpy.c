88b06a74642751037d1802b57af5a6b80730903f
Smith Ringtho <sringtho@gmail.com>
2022-03-24 04:41:53 -0700

    minor changes to task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,20 +4,18 @@
  * _strncpy - copies a string just like strncpy
  * @src: string to be appended at the end of dest
  * @dest: string to whic src is appended
- * @n: the number of bytes 
+ * @n: the number of bytes
+ * Return: pointer to dest 
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
-	{
 		dest[i] = src[i];
-	}
 
 	for ( ; i < n; i++)
-	{
 		dest[i] = '\0';
-	}
+	
 	return (dest);
 }
