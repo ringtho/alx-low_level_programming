db28682e0ff98fb149704e72ef9e39fed86af173
Smith Ringtho <45793345+ringtho@users.noreply.github.com>
2022-03-24 16:17:07 +0300

    Update task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,32 +1,34 @@
 #include "main.h"
 
 /**
- * cap_string - capitalize all words of a string
- * @str: input string
- * Return: capitalized string
+ * cap_string - capitalizes everey word of a string
+ * @s: string to modify
+ *
+ * Return: the resulting string
  */
-char *cap_string(char *str)
+char *cap_string(char *s)
 {
 	int i, j;
 
-	char spe[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+	char spe[13] = {' ', '\t', '\n', ',', ';', '.',
+		'!', '?', '"', '(', ')', '{', '}'};
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
-			str[i] -= 32;
+		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
 
 		for (j = 0; j < 13; j++)
 		{
-			if (str[i] == spe[j])
+			if (s[i] == spe[j])
 			{
-				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
 				{
-					str[i + 1] -= 32;
+					s[i + 1] -= 32;
 				}
 			}
 		}
-
 	}
-	return (str);
+
+	return (s);
 }
