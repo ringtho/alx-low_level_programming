b4dd804bc078e4d1816f4e6b0ad71547038400a2
Smith Ringtho <sringtho@gmail.com>
2022-03-24 06:47:20 -0700

    100rot

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string to rot13
+ * @str: string to be encoded
+ * Return: encoded string
+ */
+char *rot13(char *s)
+{
+	int i, j;
+
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; a[j] != '\0'; j++)
+		{
+			if (str[i] == a[j])
+			{
+				str[i] = b[j];
+				break;
+			}
+		}
+	}
+
+	return (str);
+}
