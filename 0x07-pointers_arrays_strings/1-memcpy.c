157528994bb93cab08173259f9dfa9deed125aa9
Smith Ringtho <sringtho@gmail.com>
2022-03-28 10:21:25 -0700

    1- memcpy update to fix issues

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -5,7 +5,6 @@
  * @dest: destination memory area
  * @src: memory area to copy from
  * @n: number of bytes to copy
- *
  * Return: pointer to dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
@@ -13,9 +12,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
-	{
 		dest[i] = src[i];
-	}
 
 	return (dest);
 }
