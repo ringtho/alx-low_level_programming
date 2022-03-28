b9700921c1c3299dde6848297938e7b457eac858
Smith Ringtho <sringtho@gmail.com>
2022-03-28 10:25:39 -0700

    update task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,10 +2,9 @@
 
 /**
  * *_memset - fills memory with a constant byte
- * @s: memory area to be filled
+ * @s: memory are to be filled
  * @b: char to copy
  * @n: number of times to copy b
- *
  * Return: pointer to the memory area s
  */
 char *_memset(char *s, char b, unsigned int n)
@@ -13,9 +12,7 @@ char *_memset(char *s, char b, unsigned int n)
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
-	{
 		s[i] = b;
-	}
 
 	return (s);
 }
