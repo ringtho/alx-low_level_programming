13ed0f41aef909943d1c2341c934ec1ebda4180f
Smith Ringtho <45793345+ringtho@users.noreply.github.com>
2022-03-28 19:56:23 +0300

    Update Task 0 again

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,6 +5,7 @@
  * @s: memory area to be filled
  * @b: char to copy
  * @n: number of times to copy b
+ *
  * Return: pointer to the memory area s
  */
 char *_memset(char *s, char b, unsigned int n)
