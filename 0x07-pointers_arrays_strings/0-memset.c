035e15d995d9d7a9f1f8e97e14c9d2cad10a6ec1
Smith Ringtho <45793345+ringtho@users.noreply.github.com>
2022-03-28 19:41:00 +0300

    Fix Task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -12,12 +12,10 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	i = 0;
-
-	while (i < n)
+	for (i = 0; i < n; i++)
 	{
-		*(s + i) = b;
-		i++;
+		s[i] = b;
 	}
+
 	return (s);
 }
