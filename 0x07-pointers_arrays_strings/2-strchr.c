f1b51ab91e1440b97c83db2549a775c6c19a23d6
Smith Ringtho <sringtho@gmail.com>
2022-03-28 11:44:55 -0700

    Updated task 2 to fix errors

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,24 +5,22 @@
  * *_strchr - locates a character in a string
  * @s: string to search
  * @c: char to find
- *
- * Return: a pointer to the first occurrence of the character
- * c in the string s, or NULL if the character is not found
+ * Return: a pointer to the first occurence of the character
  */
 char *_strchr(char *s, char c)
 {
-		int a;
+	int a;
 
-		while (1)
+	while (1)
+	{
+		a = *s++;
+		if (a == c)
 		{
-			a = *s++;
-			if (a == c)
-			{
-				return (s - 1);
-			}
-			if (a == 0)
-			{
-				return (NULL);
-			}
+			return (s - 1);
 		}
+		if (a == 0)
+		{
+			return (NULL);
+		}
+	}
 }
