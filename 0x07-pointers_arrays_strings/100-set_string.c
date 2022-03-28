a61f9beee18cd9346a8b895d6ca8a7f007ea64b0
Smith Ringtho <sringtho@gmail.com>
2022-03-28 12:36:00 -0700

    Fix errors in task 100

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * set_string - sets the value of a pointer to a char
- * @s: pointer to pointer
- * @to: string to set to 
+ * set_string - set the value of a pointer to a char
+ * @s: pointer to a pointer
+ * @to: pointer to string
+ * Return: void
  */
 void set_string(char **s, char *to)
 {
